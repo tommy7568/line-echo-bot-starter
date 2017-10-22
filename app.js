05324d0194d7e41550981b88a3b9d08d2fbea236
tommy7568 <32811561+tommy7568@users.noreply.github.com>
2017-10-22 17:08:32 +0800

    update issue

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let bodyParser = require('body-parser')
 let request = require('request')
 let app = express()
 
-const CHANNEL_ACCESS_TOKEN = 'Your_Channel_Access_Token'
+const CHANNEL_ACCESS_TOKEN = 'vc8M9NWwvT6NUqegf51p+UblCMZzp5eAALp8lartXvM4ufCqCWII+RYkEkoRALNi4xzzIsyIkA1Sv1hYIukHSufriIanOT0ieDeTU2hHXfmXGUpA2xCEhe7JMEfXWC8FJfaT6jG+ztVu/MVUa3qUUQdB04t89/1O/w1cDnyilFU='
 const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
