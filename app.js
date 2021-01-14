2a57f4406ec39dcc5817ef36361343127cde2ef5
tommy7568 <32811561+tommy7568@users.noreply.github.com>
2021-01-14 10:57:53 +0800

    Update app.js
    say hello 20210114

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.post('/', function (req, res) {
         let type = event.message.type
         if (type === 'text'){
             let text = event.message.text
+            if(text === "hi"){
+                sendMessage(replayToken,"HELLO")
+            }            
             sendMessage(replayToken,text)
         } else {
             sendMessage(replayToken,type)
